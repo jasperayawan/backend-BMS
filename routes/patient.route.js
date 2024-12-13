441f7808c59796962b310218f7c46574b5b10fc1
jasperayawan <ejayawan22@gmail.com>
2024-12-14 03:29:24 +0800

    update backend

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -1,8 +1,9 @@
-const { createNewPatient, getAllPatients } = require('../controller/patient.controller');
+const { createNewPatient, getAllPatients, getPatientById } = require('../controller/patient.controller');
 
 const router = require('express').Router();
 
 router.post('/', createNewPatient)
 router.get('/', getAllPatients)
+router.get('/:id', getPatientById)
 
 module.exports = router;
